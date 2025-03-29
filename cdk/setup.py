72f0b603b38442785705fb9a0d5a22c87b7cd9be
Joseph Moussa <joeph465@gmail.com>
2025-03-28 23:13:27 -0400

    update setup.py for cdk

--- cdk/setup.py
+++ cdk/setup.py
@@ -8,8 +8,7 @@ setuptools.setup(
     version="0.1.0",
     description="Stock Analysis Application CDK",
     author="jmoussa",
-    package_dir={"": "backend"},
-    packages=setuptools.find_packages(where="backend"),
+    packages=setuptools.find_packages(where="."),
     install_requires=[
         "aws-cdk-lib==2.118.0",
         "constructs>=10.0.0",
