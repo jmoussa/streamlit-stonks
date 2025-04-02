39f09ee737ef67e4e8a9c4a4d9bfee695e5d2ee3
Joseph Moussa <joeph465@gmail.com>
2025-04-02 17:00:19 -0400

    fix ecr

--- cdk/app.py
+++ cdk/app.py
@@ -9,7 +9,7 @@ app = App()
 # Environment variables can be passed from the CI/CD pipeline
 account = os.environ.get("CDK_DEFAULT_ACCOUNT")
 region = os.environ.get("CDK_DEFAULT_REGION", "us-east-1")
-ecr_repository_name = os.environ.get("ECR_REPOSITORY_NAME", "stock-analysis-app")
+ecr_repository_name = os.environ.get("ECR_REPOSITORY_NAME", "streamlit-service")
 container_image = os.environ.get("CONTAINER_IMAGE")  # Format: {account}.dkr.ecr.{region}.amazonaws.com/{repo}:{tag}
 domain_name = os.environ.get("DOMAIN_NAME", "")  # Optional: example.com
 certificate_arn = os.environ.get("CERTIFICATE_ARN", "")  # Optional: arn:aws:acm:{region}:{account}:certificate/{id}
