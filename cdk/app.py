97528e928690d4c826362fef531ed76e0b31d2c8
Joseph Moussa <joeph465@gmail.com>
2025-04-02 17:10:32 -0400

    include ecr instantiation

--- cdk/app.py
+++ cdk/app.py
@@ -3,6 +3,12 @@ from aws_cdk import App, Environment
 import aws_cdk as cdk
 import os
 from stacks.streamlit_service_stack import StreamlitServiceStack
+from stacks.ecr import ECRRepositoryStack
+import argparse as ap
+
+parser = ap.ArgumentParser()
+parser.add_argument("--stack", type=str, default="streamlit-service-stack")
+args = parser.parse_args()
 
 app = App()
 
@@ -15,14 +21,22 @@ domain_name = os.environ.get("DOMAIN_NAME", "")  # Optional: example.com
 certificate_arn = os.environ.get("CERTIFICATE_ARN", "")  # Optional: arn:aws:acm:{region}:{account}:certificate/{id}
 hosted_zone_id = os.environ.get("HOSTED_ZONE_ID", "")  # Optional: Route53 hosted zone ID
 
-app = cdk.App()
-StreamlitServiceStack(
-    app,
-    "streamlit-service-stack",
-    env=cdk.Environment(
-        account=app.node.try_get_context("account") or account,
-        region=app.node.try_get_context("region") or region,
-    ),
-)
+if args.stack == "ecr-stack":
+    ECRRepositoryStack(
+        app,
+        "ecr-repository-stack",
+        ecr_repository_name=ecr_repository_name,
+        env=Environment(account=account, region=region),
+    )
+
+if args.stack == "streamlit-service-stack":
+    StreamlitServiceStack(
+        app,
+        "streamlit-service-stack",
+        env=cdk.Environment(
+            account=app.node.try_get_context("account") or account,
+            region=app.node.try_get_context("region") or region,
+        ),
+    )
 
 app.synth()
---
+++ cdk/stacks/ecr.py
@@ -0,0 +1,28 @@
+import aws_cdk as cdk
+from aws_cdk import aws_ecr as ecr
+
+
+class ECRRepositoryStack(cdk.Stack):
+    def __init__(self, scope: cdk.Construct, id: str, ecr_repository_name: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        self.repository = ecr.Repository(
+            self,
+            "Repository",
+            repository_name=ecr_repository_name,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+        )
+
+        output_1 = cdk.CfnOutput(
+            self,
+            "RepositoryUri",
+            value=self.repository.repository_uri,
+            description="ECR Repository URI",
+        )
+
+        output_2 = cdk.CfnOutput(
+            self,
+            "RepositoryName",
+            value=self.repository.repository_name,
+            description="ECR Repository Name",
+        )
